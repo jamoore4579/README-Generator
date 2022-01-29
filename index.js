575b4debcbc3fb79d3d2971847cb4a8e16f382e1
Irish Moore <jasonamoore@frontier.com>
2022-01-29 09:36:34 -0500

    setup project and added packages

---
+++ index.js
@@ -0,0 +1,16 @@
+//npm packages (require modules)
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+// array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
